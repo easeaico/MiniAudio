fc2d773666a06c85618300675127917785b10986
yelcat <yelcat@gmail.com>
2024-06-15 23:16:31 +0800

    add miniaudio submodule

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MiniAudio",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MiniAudio",
+            targets: ["MiniAudio"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MiniAudio",
+            dependencies: ["MiniAudioC"]
+        ),
+        .target(
+            name: "MiniAudioC",
+            cSettings: [
+                .headerSearchPath("./miniaudio")
+            ]
+        ),
+        .testTarget(
+            name: "MiniAudioTests",
+            dependencies: ["MiniAudio"]),
+    ]
+)
---
+++ Sources/MiniAudio/MiniAudio.swift
@@ -0,0 +1,17 @@
+import MiniAudioC
+
+@_exported import struct MiniAudioC.ma_result;
+@_exported import struct MiniAudioC.ma_engine;
+@_exported import struct MiniAudioC.ma_engine_config;
+@_exported import enum MiniAudioC.ma_log_level
+
+public struct MiniAudio {
+
+}
+
+public extension MiniAudio {
+    @inlinable
+    static func ma_engine_init() {
+
+    }
+}
